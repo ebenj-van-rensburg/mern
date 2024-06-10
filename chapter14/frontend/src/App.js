3a36a8afecd572c4b68c7bed866112b8dee97ade
Eben Janse van Rensburg <ebenj.van.rensburg@gmail.com>
2024-06-10 22:19:31 +0200

    MERN Chapter 14 complete

---
+++ chapter14/frontend/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Switch, Route, Link } from "react-router-dom";
+import AddReview from "./components/add-review";
+import MoviesList from "./components/movies-list";
+import Movie from "./components/movie";
+import Login from "./components/login";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [user, setUser] = React.useState(null);
+ 
+  async function login(user = null){// default user to null 
+    setUser(user);
+  };
+ 
+  async function logout(){ 
+    setUser(null);
+  };
+
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {false ? (
+                <a onClick={logout}>Logout User</a>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App; 
